264930e23bb4d27d08e6b5e1ec6bd9f704f7c280
Patrick Siemsen <patrick.siemsen@t-online.de>
2022-04-27 10:36:41 +0200

    .

---
+++ 01_Basics/Variables1.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    double myValue = 13.37;
+
+    return 0;
+}
---
+++ 01_Basics/Variables3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    bool myBool = false; // 1Byte
+
+    char myVal1 = 10;   // 1Byte
+    short myVal2 = 55;  // 2Byte
+    int myVal3 = 222;   // 4Byte
+    long myVal4 = 1101; // 8Byte
+
+    float myVal5 = 12.0f;   // 4Byte
+    double myVal6 = 1212.5; // 8Byte
+
+    return 0;
+}
