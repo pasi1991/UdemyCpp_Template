8c99ae67de66fa9fdbcfb8b73cbb858ab32ebab6
Patrick Siemsen (pasi) <patrick.siemsen@ibeo-as.com>
2022-11-04 12:12:30 +0100

    solve game1

--- ComputerGame1/main.cc
+++ ComputerGame1/main.cc
@@ -1,87 +1,29 @@
 #include <iostream>
 #include <stdlib.h>
 
-#define LENX 10u
-#define LEFT 'a'
-#define RIGHT 'd'
-
 int main()
 {
 
-    bool repeat = true;
+    char left = 'a';
+    char right = 'd';
 
-    while (repeat)
+    char direction;
+
+    std::cout << "choose your direction!" << std::endl;
+    std::cin >> direction;
+
+    if(direction == left)
     {
-        unsigned int playerPosition = 0;
-        unsigned int start = 0;
-        unsigned int goal = LENX - 1;
-
-        char move;
-        bool finished = false;
-
-        while (!finished)
-        {
-            for (unsigned int i = 0; i < LENX; i++)
-            {
-                if (i != start && i != goal && i != playerPosition)
-                {
-                    std::cout << '.';
-                }
-                else if (i == playerPosition)
-                {
-                    std::cout << 'P';
-                }
-                else
-                {
-                    std::cout << '|';
-                }
-            }
-
-            std::cin >> move;
-            system("clear");
-
-            if (LEFT == move)
-            {
-                if (playerPosition > start)
-                {
-                    playerPosition--;
-                    std::cout << "You moved to the left!" << std::endl;
-                }
-                else
-                {
-                    std::cout << "You bounced!" << std::endl;
-                }
-            }
-            else if (RIGHT == move)
-            {
-                if (playerPosition < goal)
-                {
-                    playerPosition++;
-                    std::cout << "You moved to the right!" << std::endl;
-                }
-                else
-                {
-                    std::cout << "You bounced!" << std::endl;
-                }
-            }
-            else
-            {
-                std::cout << "wrong input!" << std::endl;
-            }
-
-            if (playerPosition == goal)
-            {
-                finished = true;
-                std::cout << "YOU WON" << std::endl;
-            }
-        }
-
-        std::cout << "Play again? (0=No, 1=Yes)" << std::endl;
-        std::cin >> repeat;
-
-        system("clear");
+        std::cout << "move left" << std::endl;
     }
-
+    else if(direction == right)
+    {
+        std::cout << "move right" << std::endl;
+    }
+    else
+    {
+        std::cout << "unkown direction" << std::endl;
+    }
 
     return 0;
 }
